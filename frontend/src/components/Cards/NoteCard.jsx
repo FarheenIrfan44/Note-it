87075fec9d8547b4875b71174cf2185e27bc7e24
FarheenIrfan44 <farheenirfan4040@gmail.com>
2025-01-25 20:27:13 +0500

    Updates

--- frontend/src/components/Cards/NoteCard.jsx
+++ frontend/src/components/Cards/NoteCard.jsx
@@ -17,30 +17,31 @@ const NoteCard = ({
     <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
       <div className="flex items-center justify-between">
         <div>
-          <h6 className="text-sm font-medium">{title}</h6>
-          <span className="text-xs text-green-700">
+          <div ><h6 className="text-lg  font-medium">{title}</h6></div>
+          
+          <span className="text-xs text-green-700 mt-2">
             {moment(date).format("Do MMM YYYY")}
           </span>
         </div>
 
         <MdOutlinePushPin
           className={`icon-btn ${
-            isPinned ? "text-[#2B85FF] " : "text-slate-300"
+            isPinned ? "text-black " : "text-slate-300"
           }`}
           onClick={onPinNote}
         />
       </div>
 
-      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+      <p className="text-sm text-slate-600 mt-2">{content?.slice(0, 50)}</p>
 
-      <div className="flex items-center justify-between mt-2">
+      <div className="flex items-center justify-between mt-4">
         <div className="text-xs text-slate-500">
           {tags.map((item) => `#${item} `)}
         </div>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-4">
           <MdCreate
-            className="icon-btn hover:text-green-600"
+            className="icon-btn hover:text-black"
             onClick={onEdit}
           />
 
