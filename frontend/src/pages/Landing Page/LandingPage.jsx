3e2860bfa8ebeffb49e4fa47d632d43c9418dee3
FarheenIrfan44 <farheenirfan4040@gmail.com>
2025-01-26 13:16:49 +0500

    updates

--- frontend/src/pages/Landing Page/LandingPage.jsx
+++ frontend/src/pages/Landing Page/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import backgroundImg from "../../assets/background.jpg";
+import backgroundImg from "../../assets/background.png";
 
 export const LandingPage = () => {
   return (
@@ -14,10 +14,12 @@ export const LandingPage = () => {
       }}
     >
       <div className="text-white text-center overflow-x-hidden">
-        <h5 className="mb-4 text-[#12296c] font-extrabold text-6xl max-w-[40rem]">Write your thoughts down here as they come to you</h5>
+        <h5 className="mb-4 text-[#12296c] font-extrabold text-6xl max-w-[50rem]">
+          Write your thoughts down here as they come to you
+        </h5>
         <a
-          href="http://localhost:5173/signup" 
-          className="m-8 border-4 border-[#12296c] bg-[#12296c] text-white text-xl p-1 px-8 rounded-full inline-block transition hover:-translate-y-2 hover:bg-[#54a889] hover:text-[#12296c]"
+          href="http://localhost:5173/signup"
+          className="m-8 border-4 border-[#12296c] bg-[#12296c] text-white text-xl p-1 px-8 rounded-full inline-block transition hover:-translate-y-2 hover:bg-[#A9CD85] hover:text-[#12296c]"
         >
           Start now! its free
         </a>
